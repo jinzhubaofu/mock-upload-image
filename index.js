e3d4e1ad2ba5d63fa34c6ea03124568a3bb3a3b3
lupengyu <lupengyu@baidu.com>
2017-07-21 11:30:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * @file mock 上传图片
+ * @author leon <ludafa@outlook.com>
+ */
+
+const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const Handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+
+let app = new Koa();
+let imageRouter = new KoaRouter();
+
+let template = Handlebars.compile(fs.readFileSync(path.join(__dirname, 'index.hbs'), 'utf8'));
+
+imageRouter.post('/image', async ctx => {
+    ctx.body = 'https://placeimg.com/640/480/animals';
+});
+
+imageRouter.get('/', async ctx => {
+    ctx.body = template({
+        title: 'test'
+    });
+});
+
+app.use(imageRouter.routes());
+app.use(imageRouter.allowedMethods());
+app.listen(8818);
+console.log('server start');
